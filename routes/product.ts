dd018b981ebb8ffd42452d1c928e75cc8e34d445
daniel.correa <daniel.correa@perficient.com>
2024-02-13 10:30:36 -0500

    Adding swagger to project

--- routes/product.ts
+++ routes/product.ts
@@ -6,6 +6,30 @@ const productController = new ProductController()
 const router = Router()
 const { getProducts } = productController
 
+
+/**
+ * TODO: fill properties
+ * @swagger
+ * /product:
+ *   get:
+ *     summary: Returns all products.
+ *     responses:
+ *       200:
+ *         description: A list of products.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: Product ID.
+ *                   name:
+ *                     type: string
+ *                     description: Product's name.
+ */
 router.get('/product', getProducts.bind(productController))
 
 export default router
---
+++ swaggerDef.ts
@@ -0,0 +1,17 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'CommerceTools SDK',
+            version: '1.0.0',
+            description: 'CommerceTools SDK example documentation',
+        },
+    },
+    apis: ['./routes/*.ts'], // Path to the API docs
+};
+
+const swaggerSpecification = swaggerJsdoc(options);
+
+export default swaggerSpecification;
