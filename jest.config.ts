d6c7fd9bf7950fb74ac3724244ee66113bede066
daniel.correa <daniel.correa@perficient.com>
2024-02-21 15:34:49 -0500

    Adding test structure and coverage. Adding multiple environment files and properties.

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,14 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'client/*.{ts,tsx}',
+        'controller/*.{ts,tsx}',
+        'repository/*.{ts,tsx}',
+        'routes/*.{ts,tsx}',
+        'utils/*.{ts,tsx}',
+    ],
 };
 
 export default config;
--- test/repository/CategoryRepository.test.ts
+++ test/repository/CategoryRepository.test.ts
@@ -1,68 +1,27 @@
-import categoryRepository from "../../repository/CategoryRepository";
-import { createClient } from '../../utils/Client';
+import server from "../../index";
+import request from 'supertest';
 
-jest.mock('../../utils/Client', () => ({
-    createClient: jest.fn(function (options) {
-        const { ApiRoot } = require('@commercetools/platform-sdk');
-        this.apiRoot = new ApiRoot();
-        this.projectKey = options.projectKey;
-    }),
-}));
+const mockedProducts = {data: 'mockProducts'}
+const mockedResponse = { statusCode:200,message:'test', body: mockedProducts }
+jest.mock('../../repository/CategoryRepository', () => {
+    return jest.fn().mockImplementation(() => {
+        return {
+            getProductsForCategory: jest.fn().mockResolvedValue(mockedResponse),
+        };
+    });
+});
 
-jest.mock('@commercetools/platform-sdk', () => ({
-    ...jest.requireActual('@commercetools/platform-sdk'), // Optional: Retain other exports from the module
-    ApiRoot: jest.fn(() => ({
-        withProjectKey: jest.fn().mockReturnThis(),
-        productProjections: jest.fn().mockReturnThis(),
-        search: jest.fn().mockReturnThis(),
-        get: jest.fn().mockReturnThis(),
-        execute: jest.fn().mockResolvedValue({ data: 'mockProducts' }),
-    })),
-    expectedProducts : { data: 'mockProducts' }
-}));
+afterAll(async () => {
+    server.close();
+});
 
 describe('CategoryRepository', () => {
-    const mockOptions = { projectKey: 'testKey' };
 
-    beforeEach(() => {
-        (createClient as jest.Mock).mockClear();
+    it('should return products for a given category ID',  async() => {
+
+        const response =  await request(server).get('/category/test');
+        expect(response.statusCode).toBe(200);
+        expect(response.body.data).toEqual(mockedProducts);
     });
 
-    it('should create a client with the provided options', () => {
-        const repository = new categoryRepository(mockOptions);
-        expect(createClient).toHaveBeenCalledWith(mockOptions);
-    });
-
-    it('should return products for a given category ID', async () => {
-        const ApiRoot = require('@commercetools/platform-sdk').ApiRoot;
-        ApiRoot.mockImplementation(() => ({
-            withProjectKey: jest.fn().mockReturnThis(),
-            productProjections: jest.fn().mockReturnThis(),
-            search: jest.fn().mockReturnThis(),
-            get: jest.fn().mockReturnThis(),
-            execute: jest.fn().mockResolvedValue(ApiRoot.expectedProducts),
-        }));
-
-        const categoryRepo = new categoryRepository(mockOptions);
-        const products = await categoryRepo.getProductsForCategory('123');
-
-        expect(products).toEqual(ApiRoot.expectedProducts);
-    });
-
-    it('should return an error if the API call fails', async () => {
-        const expectedError = new Error('API error');
-        const ApiRoot = require('@commercetools/platform-sdk').ApiRoot;
-        ApiRoot.mockImplementation(() => ({
-            withProjectKey: jest.fn().mockReturnThis(),
-            productProjections: jest.fn().mockReturnThis(),
-            search: jest.fn().mockReturnThis(),
-            get: jest.fn().mockReturnThis(),
-            execute: jest.fn().mockRejectedValue(expectedError),
-        }));
-
-        const categoryRepo = new categoryRepository(mockOptions);
-        const error = await categoryRepo.getProductsForCategory('123');
-
-        expect(error).toBe(expectedError);
-    });
 });
